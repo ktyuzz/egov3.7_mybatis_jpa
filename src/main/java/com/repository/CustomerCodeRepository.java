a9e1c4a7ce3d73eb9c1a9b6885629bb35e6b4058
KTYUZZ\ktyuzz <ktyuzz@ecis.co.kr>
2018-04-04 17:49:49 +0900

    1. 불용 리소스 제거 2. JPA xsd 스키마 변경 3. 불필요한 pom.xml내 주석 제거

--- src/main/java/com/repository/CustomerCodeRepository.java
+++ src/main/java/com/repository/CustomerCodeRepository.java
@@ -3,7 +3,6 @@ package com.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.domain.CustomerCode;
 
-//@RepositoryRestResource(collectionResourceRel = "customercodes", path = "customercodes")
 public interface CustomerCodeRepository extends JpaRepository<CustomerCode, Integer> {
     
 }
